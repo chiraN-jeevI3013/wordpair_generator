dba7b0e9bf404642ed407deb7e747019ce79a030
CJ <chiran.jeevi3013@gmail.com>
2025-10-26 00:06:54 -0700

    Added SavedWords functionality & Page

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -51,9 +51,41 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
+  void _pushSaved () {
+    // Navigate to another page
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _savedWordPairs.map((WordPair pair) {
+            return ListTile(
+              title: Text(pair.asPascalCase, style: TextStyle(fontSize: 16))
+            );
+          });
+
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved WordPairs'),
+            ),
+            body: ListView(children: divided),
+          );
+        }
+      )
+    );
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('WordPair Generator')),
+        appBar: AppBar(
+          title: Text('WordPair Generator'),
+          actions: <Widget>[
+            IconButton(onPressed: _pushSaved, icon: Icon(Icons.list))
+          ],
+        ),
         body: _buildList()
       );
   }
